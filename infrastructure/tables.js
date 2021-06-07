c4624a9bcbb382c83ddf1a56cdb06aa9aa4f0414
Thiago Sousa <thiagosvin@hotmail.com>
2021-06-07 04:47:32 -0300

    Serviços

--- infrastructure/tables.js
+++ infrastructure/tables.js
@@ -2,34 +2,32 @@ class Tables {
   init(connection) {
     this.connection = connection;
 
-    this.createAttendance();
-    this.createPets();
+    this.createAttendance()
+    this.createPets()
   }
 
   createAttendance() {
-    const sql =
-      "CREATE TABLE IF NOT EXISTS Attendance (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), services varchar(20) NOT NULL, date datetime NOT NULL, creationDate datetime NOT NULL, status varchar(20) NOT NULL, notes text, PRIMARY KEY(id) )";
-
+    const sql = 'CREATE TABLE IF NOT EXISTS Attendance (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), services varchar(20) NOT NULL, date datetime NOT NULL, creationDate datetime NOT NULL, notes text, PRIMARY KEY(id) )'
+    
     this.connection.query(sql, (erro) => {
-      if (erro) {
-        console.log(erro);
-      } else {
-        console.log("Tabela atendimentos criada com sucesso");
-      }
+        if (erro) {
+            console.log(erro);
+        }else{
+            console.log('Tabela atendimentos criada com sucesso');
+        }
     });
   }
 
-  createPets() {
-    const query =
-      "CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, name varchar(50), image varchar(200), PRIMARY KEY (id) ) ";
+  createPets(){
+    const query = 'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, name varchar(50), image varchar(200), PRIMARY KEY (id) ) '
 
-    this.connection.query(query, (erro) => {
-      if (erro) {
+    this.connection.query(query, erro => {
+      if(erro){
         console.log(erro);
-      } else {
-        console.log("Tabela Pets foi criada com sucesso");
+      }else{
+        console.log('Tabela Pets foi criada com sucesso');
       }
-    });
+    })
   }
 }
 module.exports = new Tables();
--- models/attendances.js
+++ models/attendances.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const { json } = require("body-parser");
 const moment = require("moment");
 const connection = require("../infrastructure/connection");
@@ -58,11 +59,14 @@ class Attendance {
 
   searchById(id, res){
     const sql = `SELECT * FROM Attendance WHERE id=${id}`
-    connection.query(sql, (erro, results) =>{
+    connection.query(sql, async (erro, results) =>{
       const attendance = results[0]
+      const cpf = attendance.customer
       if (erro) {
         res.status(400).json(erro)
       }else{
+        const {data} = await axios.get(`http://localhost:8082/${cpf}`)
+        attendance.customer = data;
         res.status(200).json(attendance)
       }
     })
